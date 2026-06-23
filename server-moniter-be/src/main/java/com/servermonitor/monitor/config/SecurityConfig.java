2968062065c792aec00f3e7517a6ffbc2443c8b1
mg-dotcom <apisara.chon@gmail.com>
2026-06-23 16:44:21 +0700

    update CORS configuration to allow requests from the production URL

--- server-moniter-be/src/main/java/com/servermonitor/monitor/config/SecurityConfig.java
+++ server-moniter-be/src/main/java/com/servermonitor/monitor/config/SecurityConfig.java
@@ -49,8 +49,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://server-moniter.vercel.app"
+        ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
