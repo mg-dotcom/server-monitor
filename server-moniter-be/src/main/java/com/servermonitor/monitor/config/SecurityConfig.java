9268bccc15ba7949c304082da41ee48b4a6efa1f
mg-dotcom <apisara.chon@gmail.com>
2026-06-23 20:06:13 +0700

    add new allowed origin for CORS configuration

--- server-moniter-be/src/main/java/com/servermonitor/monitor/config/SecurityConfig.java
+++ server-moniter-be/src/main/java/com/servermonitor/monitor/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "https://server-moniter.vercel.app"
+                "https://server-moniter.vercel.app",
+                "https://server-monitor-git-main-mg-dotcoms-projects.vercel.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
